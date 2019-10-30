42a5b21cc418cb8ee3439d52408d5df44ed4dba3
liw <liw@sina.com>
2019-10-30 16:59:29 +0800

    tomcat war deploy

--- src/main/java/com/huatec/ventpipe/Application.java
+++ src/main/java/com/huatec/ventpipe/Application.java
@@ -2,12 +2,21 @@ package com.huatec.ventpipe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	//支持tomcat war部署
+	@Override
+	protected SpringApplicationBuilder configure(
+			SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
 	}
 
 }
